a931375c26a933da8eadb182c4409d1d9f2ccf91
karthick2311 <karthick23111999.a@gmail.com>
2023-07-21 08:58:23 +0530

    puppy changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/karthick2311/puppy
 
 go 1.20
+
+require github.com/karthick2311/dog v0.0.0-20230720145513-a98ba410c0e7 // indirect
--- puppy.go
+++ puppy.go
@@ -1,5 +1,8 @@
 package puppy
 
+import (
+	"github.com/karthick2311/dog"
+)
 
 func Bark() string {
 	return "Woof!"
@@ -7,4 +10,11 @@ func Bark() string {
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
